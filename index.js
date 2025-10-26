5fff82685704510983a6475b261dd49e48bc64d2
dvstechy <work.divyashah@gmail.com>
2025-10-26 10:21:12 +0530

    transporter changed

--- index.js
+++ index.js
@@ -23,12 +23,15 @@ function generateOtp() {
 // 🔹 Utility: Send Email
 async function sendEmail(to, otp) {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
-    },
-  });
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // TLS
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
+
 
   const mailOptions = {
     from: `"Heritage Bites" <${process.env.SMTP_USER}>`,
